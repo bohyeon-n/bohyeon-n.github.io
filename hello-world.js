c7fe63a6d8d889fc22dcd6718795c722a8c3a01d
koobohyeon <goosee115@gmail.com>
2018-07-18 16:00:58 +0900

    seperate html component

--- hello-world.js
+++ hello-world.js
@@ -2,24 +2,44 @@
 const path = require("path");
 const http = require("http");
 const fs = require("fs");
-
+const ejs = require("ejs");
+const ejsLint = require("ejs-lint");
 //joining path of directory
 let directoryPath = path.join(__dirname, "content");
 //passsing directoryPath and callback function
 
-directories = fs.readdirSync(directoryPath);
-directoryFiles = [];
-directories.forEach(directory => {
-  const fileDirectoryPath = `${directoryPath}/${directory}`;
-  files = fs.readdirSync(fileDirectoryPath);
-  files.forEach(f => directoryFiles.push(f));
+let fileLists = [];
+let directories = fs.readdirSync(directoryPath);
+directories.forEach((directory, index) => {
+  fileLists.push(fs.readdirSync(`./content/${directory}`));
+});
+console.log(fileLists);
+
+const indexHtmlFormat = fs.readFileSync("./public/index.html", "utf8");
+const sidebarHtmlFormat = fs.readFileSync("./public/sidebar.html", "utf8");
+const mainHtmlFormat = fs.readFileSync("./public/main.html", "utf8");
+const homeHtmlFormat = fs.readFileSync("./public/home.html", "utf8");
+
+let sidebar = ejs.render(sidebarHtmlFormat, {
+  folderList: directories
 });
 
-let ejs = require("ejs");
-var indexHtmlFormat = fs.readFileSync("./public/index.html", "utf-8");
+fileLists.forEach((fileList, index) => {
+  let main = ejs.render(mainHtmlFormat, {
+    fileList: fileList,
+    folderName: directories[index]
+  });
+  let html = ejs.render(indexHtmlFormat, {
+    folderList: directories,
+    main: main,
+    sidebar: sidebar
+  });
+
+  fs.writeFileSync(`./deploy/${directories[index]}.html`, html);
+});
+
 html = ejs.render(indexHtmlFormat, {
-  title: "aaaa",
-  fileList: directoryFiles
+  sidebar: sidebar,
+  main: homeHtmlFormat
 });
-
-fs.writeFileSync("./deploy/index.html", html);
+fs.writeFileSync("./deploy/home.html", html);
