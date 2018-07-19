f08a7ddff4f139d127f3738aebd6aef78ee100f4
koobohyeon <goosee115@gmail.com>
2018-07-19 13:29:01 +0900

    add sideabr to article

--- hello-world.js
+++ hello-world.js
@@ -22,7 +22,6 @@ const indexHtmlFormat = fs.readFileSync("./public/index.html", "utf8");
 const sidebarHtmlFormat = fs.readFileSync("./public/sidebar.html", "utf8");
 const mainHtmlFormat = fs.readFileSync("./public/main.html", "utf8");
 const homeHtmlFormat = fs.readFileSync("./public/home.html", "utf8");
-const articleHtmlFormat = fs.readFileSync("./public/article.html", "utf8");
 
 let sidebar = ejs.render(sidebarHtmlFormat, {
   folderList: directories
@@ -39,7 +38,7 @@ fileLists.forEach((fileList, index) => {
     sidebar: sidebar
   });
 
-  fs.writeFileSync(`./deploy/${directories[index]}.html`, html);
+  fs.writeFileSync(`./deploy/${directories[index]}-index.html`, html);
   fileList.forEach(file => {
     // markdown to html file
     const markdownFile = fs.readFileSync(
@@ -48,12 +47,13 @@ fileLists.forEach((fileList, index) => {
     );
     let convertedFile = md.render(markdownFile);
 
-    let html = ejs.render(articleHtmlFormat, {
-      article: convertedFile
+    let html = ejs.render(indexHtmlFormat, {
+      main: convertedFile,
+      sidebar: sidebar
     });
     let n = file.indexOf(".");
     let fileName = file.slice(0, n);
-    fs.writeFileSync(`./deploy/content/${fileName}.html`, html);
+    fs.writeFileSync(`./deploy/${fileName}.html`, html);
   });
 });
 
